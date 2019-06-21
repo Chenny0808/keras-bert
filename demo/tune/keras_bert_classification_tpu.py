fb1bc3be9fbd6559f13a1ea5cff253a5f61808db
gpc <15927299723@163.com>
2019-06-21 11:52:23 +0800

    add a py file

---
+++ demo/tune/keras_bert_classification_tpu.py
@@ -0,0 +1,9 @@
+#! user/bin/env python
+# _*_ encoding: utf-8 _*_
+"""
+ @File Name: keras_bert_classification_tpu
+ @Author:    Chenny
+ @email:     15927299723@163.com
+ @date:      2019/6/21 11:51
+ @software:  PyCharm
+"""
